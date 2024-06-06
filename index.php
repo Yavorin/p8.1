4228afdc4db470fce9ff31a3c44757ba30aa30a7
Yavorin <159899546+Yavorin@users.noreply.github.com>
2024-06-06 13:40:46 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="css/style.css">
+    <title>Суперглобальні змінні PHP</title>
+</head>
+<body>
+<h1 class="title_style">Суперглобальні змінні PHP</h1>
+
+<table>
+    <tr>
+        <th>Позначення змінної</th>
+        <th>Характеристика</th>
+        <th>Отримане значення</th>
+    </tr>
+    <tr>
+        <td>$GLOBALS</td>
+        <td>Зберігає всі глобальні змінні</td>
+        <td><?php print_r($GLOBALS); ?></td>
+    </tr>
+    <tr>
+        <td>$_SERVER</td>
+        <td>Інформація про сервер і середовище</td>
+        <td><?php print_r($_SERVER); ?></td>
+    </tr>
+    <tr>
+        <td>$_GET</td>
+        <td>Параметри, передані методом GET</td>
+        <td><?php print_r($_GET); ?></td>
+    </tr>
+    <tr>
+        <td>$_POST</td>
+        <td>Параметри, передані методом POST</td>
+        <td><?php print_r($_POST); ?></td>
+    </tr>
+    <tr>
+        <td>$_FILES</td>
+        <td>Інформація про завантажені файли</td>
+        <td><?php print_r($_FILES); ?></td>
+    </tr>
+    <tr>
+        <td>$_COOKIE</td>
+        <td>Змінні, збережені в кукі</td>
+        <td><?php print_r($_COOKIE); ?></td>
+    </tr>
+    <tr>
+        <td>$_REQUEST</td>
+        <td>Змінні HTTP-запиту ($_GET, $_POST і $_COOKIE об'єднані)</td>
+        <td><?php print_r($_REQUEST); ?></td>
+    </tr>
+    <tr>
+        <td>$_ENV</td>
+        <td>Інформація про змінні середовища</td>
+        <td><?php print_r($_ENV); ?></td>
+    </tr>
+</table>
+</body>
+</html>
